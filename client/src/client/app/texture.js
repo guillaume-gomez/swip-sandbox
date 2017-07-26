9852f5d85adbfe1d50033bfe7e86ce281de630b3
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-07-26 19:20:45 +0200

    fix indent

--- client/src/client/app/texture.js
+++ client/src/client/app/texture.js
@@ -1,9 +1,9 @@
 //Tomahawk.registerClass( Texture, "Texture" );
 class Texture {
-	constructor() {
-		this.data = null;
-		this.name = null;
-		this.rect = null;
-	}
+  constructor() {
+    this.data = null;
+    this.name = null;
+    this.rect = null;
+  }
 };
 export default Texture;
