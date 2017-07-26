8c4770334a8bfc127e6c130baf90aa793ab05a51
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-07-27 00:49:24 +0200

    check the code made recently

---
+++ client/test/sample.js
@@ -0,0 +1,10 @@
+/* Point d'entrée de l'application */
+function init(){
+    //code de notre Application
+}
+
+/* 
+* Quand toutes les données sont chargées ( DOM, Images, Sons, Vidéos etc ... )
+* On démarre l'application par la fonction init
+*/
+window.onload = init;
