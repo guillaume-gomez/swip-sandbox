1f11ce383f6a08c9e5f69975cad6875e5f3bffed
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-07-27 14:07:53 +0200

    change fps displayer

--- client/src/client/app/stage.js
+++ client/src/client/app/stage.js
@@ -2,76 +2,70 @@
 // Tomahawk.extend( "Stage", "DisplayObjectContainer" );
 import DisplayObjectContainer from "./displayObjectContainer";
 
+let instance = null;
+
 class Stage extends DisplayObjectContainer {
   constructor() {
-    window.requestAnimationFrame = (function(){  
-      return  window.requestAnimationFrame       ||  //Chromium 
-              window.webkitRequestAnimationFrame ||  //Webkit
-              window.mozRequestAnimationFrame    || //Mozilla Geko
-              window.oRequestAnimationFrame      || //Opera Presto
-              window.msRequestAnimationFrame     || //IE Trident?
-              function(callback, element){ //Fallback function
-                  window.setTimeout(callback, 10);                
-              }
-      });
+    super();
+    if(!instance) {
+      instance = this;
+    }
     this._lastTime = 0;
     this._frameCount = 0;
     this._fps = 0;
     this._canvas = null;
     this._context = null;
     this._debug = false;
+  }
 
-    super();
+  myRequestAnimationFrame(){
+      return  window.requestAnimationFrame       ||  //Chromium
+              window.webkitRequestAnimationFrame ||  //Webkit
+              window.mozRequestAnimationFrame    || //Mozilla
+              window.oRequestAnimationFrame      || //Opera
+              window.msRequestAnimationFrame     || //IE
+              function(callback, element){ //Fallback function
+                  window.setTimeout(callback, 10);
+              }
   }
 
   getInstance() {
-    if(this._instance === null) {
-      this._instance = new Stage();
-    }  
-    return this._instance;
+    if(!instance ) {
+      instance = this;
+    }
+    return this;
   }
 
-  init(canvas) {    
+  init(canvas) {
     this._canvas = canvas;
     this._context = canvas.getContext("2d");
-    this._enterFrame();        
+    this._enterFrame();
   }
 
   _enterFrame() {
-      
-      var curTime = new Date().getTime();
-      var scope = this;
-      
+      const curTime = new Date().getTime();
       this._frameCount++;
-      
       if(curTime - this._lastTime >= 1000){
           this._fps = this._frameCount;
           this._frameCount = 0;
           this._lastTime = curTime;
       }
-      
       this._context.clearRect(0, 0, this._canvas.width, this._canvas.height);
       this._context.save();
       this.render(this._context);
       this._context.restore();
-      
-      if(this._debug == true){
+      if(this._debug === true){
           this._context.save();
           this._context.beginPath();
           this._context.fillStyle = "black";
           this._context.fillRect(0,0,100,30);
           this._context.fill();
-          this._context.fillStyle = "red";
-          this._context.font = 'italic 20pt Calibri';
+          this._context.fillStyle = "#016701";
+          this._context.font = "18pt sans-serif";
           this._context.fillText("fps: "+this._fps, 0,30);
           this._context.restore();
       }
-      
-      window.requestAnimationFrame(
-          function(){
-              scope._enterFrame();
-          }
-      );
+      this.myRequestAnimationFrame()( () => { this._enterFrame(); });
   }
 
   getCanvas() {
