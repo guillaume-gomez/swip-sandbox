7f9c7524db911938ec114a039550157b8c7ce589
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-07-28 23:09:56 +0200

    add missing method, cannot override in JS

--- client/src/client/app/displayObjectContainer.js
+++ client/src/client/app/displayObjectContainer.js
@@ -9,6 +9,14 @@ class DisplayObjectContainer extends DisplayObject {
       this.children = [];
   }
 
+  addChild(child) {
+    if(child.parent) {
+      child.parent.removeChild(child);
+    }
+    child.parent = this;
+    this.children.push(child);
+  }
+
   draw(context) {
     this.children.forEach(child => {
       child.render(context);
